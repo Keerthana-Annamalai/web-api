09dbfbaf9614e256904f5045acf6f77a726d0fce
dharanigowthamsampath <dharanigowthamsampath@gmail.com>
2024-11-19 11:38:18 +0530

    created package for controller

--- src/main/java/com/pipercadd/api/controller/HelloWorldController.java
+++ src/main/java/com/pipercadd/api/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.pipercadd.api;
+package com.pipercadd.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +8,11 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, World!";
+        return "Hello, World! User";
     }
 
     @GetMapping("/")
     public String home() {
-        return "Welcome to the homepage!";
+        return "Welcome to the homepage! User";
     }
 }
